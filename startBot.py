13f5be6de870996245fc8dd2713efb72e3b5a0ec
peter <od130373ppa@gmail.com>
2017-01-10 17:42:10 +0200

    This is working!!!

--- startBot.py
+++ startBot.py
@@ -1,8 +1,10 @@
 import os
-os.environ['GLOG_minloglevel'] = '4'
+os.environ['GLOG_minloglevel'] = '5'
+
+import warnings
+warnings.filterwarnings('ignore')
 
 from Classes import TelegramBot
 
 temp = TelegramBot()
 
-temp.start()
